19c365a2c2a5d6bf162cd58dfd17340d9d8882e1
Kamila Pochojka <kamila.pochojka@arithnea.de>
2020-07-16 16:06:36 +0200

    burger-menu

---
+++ source/assets/js/components/burger-menu.js
@@ -0,0 +1,12 @@
+const burgerAnimation = () => {
+    const burgerMenu = document.querySelector('#burger-menu');
+    const header = document.querySelector('header');
+    if(burgerMenu) {
+        burgerMenu.addEventListener('click', function () {
+            header.classList.toggle("open");
+        })
+    }
+
+};
+
+export default burgerAnimation;
