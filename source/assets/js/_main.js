fbff728600813ee000798fd8387ce6830a549d88
Kamila Pochojka <kamila.pochojka@arithnea.de>
2020-07-16 20:01:32 +0200

    building slider

--- source/assets/js/_main.js
+++ source/assets/js/_main.js
@@ -1,7 +1,9 @@
 import headerScroll from "./components/header";
 import burgerAnimation from "./components/burger-menu";
+import slider from "./components/slider";
 
 (function() {
    // headerScroll();
     burgerAnimation();
+    slider();
 })();
---
+++ source/assets/js/components/slider.js
@@ -0,0 +1,30 @@
+import $ from "jquery";
+import 'slick-carousel/slick/slick';
+
+const slider = () => {
+    $('.teaser-triple__content').slick({
+        slide: '.teaser-triple__item',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        rows: 0,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 9999,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 1025
+            }
+        ]
+    });
+
+    $(window).on('resize orientationchange', function() {
+        $('.teaser-triple__content').slick('resize');
+    });
+};
+
+export default slider;
